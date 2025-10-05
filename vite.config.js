d510c51343000a3fdac142cb948b3f2e90d25b05
bubcass <81823823+bubcass@users.noreply.github.com>
2025-10-05 12:47:49 +0100

    Add vite.config.js in project root for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 replace with your actual repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/stor-publisher/',
 })
